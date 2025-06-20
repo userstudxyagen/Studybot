5feaa14defef4aa68b6fd603567a1fef79e927e0
userstudxyagen <husseinrooni@gmail.com>
2025-06-20 13:49:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from webscraper import scrape_text_from_url  # type: ignore
 from pdf_handler import extract_text_from_pdf # type: ignore
 from coding_helper import get_coding_help # type: ignore
 from math_helper import solve_math_problem # type: ignore
-from agent import ask_deepseek # type: ignore
+from agent import ask_model # type: ignore
 
 
 # Modernes Layout
